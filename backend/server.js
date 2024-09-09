0bb455fdb2910607f9e3204769f85e9a7c2fb9b1
Deepak singh <deepaksingh1realme@gmail.com>
2024-09-09 20:52:28 +0530

    Deployment1

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 import authRoutes from "./routes/authroutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -12,20 +13,26 @@ import { app } from './socket/socket.js'
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve()
+
 dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
 
-
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
-
-
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "frontend/dist")));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+app.use((req, res, next) => {
+    console.log(`Incoming request: ${req.method} ${req.url}`);
+    next();
+});
+
 
 const startServer = async () => {
     try {
