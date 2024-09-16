2156db424494e804ab5e9216e50b6b66fef16f7b
Deepak Singh <162002289+Deepak-singh077@users.noreply.github.com>
2024-09-16 11:13:52 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: ["http://localhost:3000"],
+		origin: ["https://mern-chat-app-e95e.onrender.com"],
 		methods: ["GET", "POST"],
 	},
 });
