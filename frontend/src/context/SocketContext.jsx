e57564f5e7fe1608690f5873a64cca0d4e8064b3
Deepak Singh <162002289+Deepak-singh077@users.noreply.github.com>
2024-09-16 11:02:15 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://mern-chat-app-qrdm.onrender.com", {
+			const socket = io("https://mern-chat-app-e95e.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
